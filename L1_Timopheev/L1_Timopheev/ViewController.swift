30ce0b9c0b1ee5d6823ba527e2bb17adf85794c5
Nikitator-2 <timopheev@yandex.ru>
2019-03-02 00:17:07 +0300

    Первое задание. Магию клавиатуры не освоил. Копирование методички не дало рабочего кода.... Надеюсь на разбор перед уроком. И очень прошу, сделайте больше рабочую область. Очень тяжело пересматривать уже записанное видео. Все очень мелко.

---
+++ L1_Timopheev/L1_Timopheev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L1_Timopheev
+//
+//  Created by NIKITA TIMOFEEV on 01/03/2019.
+//  Copyright © 2019 NIKITA TIMOFEEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
