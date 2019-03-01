4bb827f1c6c6b3117d6f8b77065b949f485e201b
Nikitator-2 <timopheev@yandex.ru>
2019-03-02 00:20:06 +0300

    Магию клавиатуры не осилил. КОд из методички не зашел. Надеюсь на разюор на уроке.

--- L1_Timopheev/L1_Timopheev/ViewController.swift
+++ L1_Timopheev/L1_Timopheev/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+   
+    @IBAction func signIn(_ sender: Any) {
+        if let login = loginTextField.text,
+            let password = passwordTextField.text {
+    
+            if login == "admin" && password == "12345" {
+                print("Acess allowed!")
+            } else {
+                    print("You shall not pass!!!")
+            }
+            
+        }
 }
+
+    
+    
+}
+
 
